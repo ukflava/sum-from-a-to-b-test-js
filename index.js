5c428693a577e53c421aae7dbbd171b671a60546
ukflava <ukflava@gmail.com>
2022-06-05 07:44:50 -0700

    absumRecursion

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return toN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
 }
+
+console.log(sum(4, 8));
+
+// Sum all the values from fromN up to toN
+
 
 module.exports = sum;
